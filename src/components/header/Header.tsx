5a09dda072aaf889fb98017c8593c3953d009b1e
yeytaken <yeytaken.js@gmail>
2024-12-25 19:23:05 -0300

    Feat: add responsive Header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,7 @@
 'use client';
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { MdOutlineOpenInNew } from "react-icons/md";
+import {  MdMenu, MdOutlineOpenInNew } from "react-icons/md";
 
 /**
  * @this target="_blank" abre uma nova guia ao envés de redirecionar como href="/"
@@ -10,7 +10,11 @@ export const Header = () => {
   const currentPath = usePathname();
 
   return (
-    <nav className="flex items-center justify-center bg-primary py-4">
+    <nav className="flex items-center justify-start md:justify-center bg-primary py-2 sm:py-4 px-6">
+      <button className="sm:hidden">
+        <MdMenu size={24} />
+      </button>
+
       <ul className="flex gap-4 items-center">
         <li className="my-2">
           <Link href="/" className="border-2 rounded-md py-2 px-1 font-bold">
@@ -18,25 +22,27 @@ export const Header = () => {
           </Link>
         </li>
         
-        <li>
+        <li className="hidden sm:block">
           <Link href="/" data-active={currentPath === "/"} className="data-[active=true]:underline">
           Página inicial
           </Link>
         </li>
 
-        <li>
+        <li className="hidden sm:block">
         <Link href="/cursos" data-active={currentPath === "/cursos"} className="data-[active=true]:underline">
         Cursos
         </Link>
         </li>
 
-        <li>
+        <li className="hidden sm:block">
           <Link href="https://google.com/" target="_blank" className="flex gap-1 items-center">
             Blog
             <MdOutlineOpenInNew />
           </Link>
         </li>
       </ul>
+
+      <h1 className="sm:hidden">Taken Coders - Página inicial</h1>
     </nav>
   );
 };

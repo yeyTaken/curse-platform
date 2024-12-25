500a96a61d813611b53b6c5a7857bd095c267074
yeytaken <yeytaken.js@gmail>
2024-12-25 20:20:00 -0300

    Feat: mobile render

--- src/app/cursos/page.tsx
+++ src/app/cursos/page.tsx
@@ -1,4 +1,10 @@
+import type { Metadata } from "next";
 
+
+export const metadata: Metadata = {
+  title: "Taken Coders - Todos os cursos",
+  description: "Veja todos os cursos da Taken Coders.",
+};
 
 export default function Cursos() {
   return (
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import type { Metadata } from "next";
+
+
+export const metadata: Metadata = {
+  title: "Taken Coders - Página inicial",
+  description: "A melhor plataforma de cursos GRATUITOS de todos os tempos.",
+};
+
 export default function Home() {
   return (
     <main>

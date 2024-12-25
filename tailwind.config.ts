a1fb43e477f6eadc99210d8744868368d2404939
yeytaken <yeytaken.js@gmail>
2024-12-25 16:57:45 -0300

    Feat: Add theme light and dark

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,16 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    extend: {
+      colors: {
+        text: 'var(--color-text)',
+        error: 'var(--color-error)',
+        paper: 'var(--color-paper)',
+        primary: 'var(--color-primary)',
+        background: 'var(--color-background)',
+        'primary-contrast': 'var(--color-primary-contrast)',
+      }
+    },
   },
   plugins: [],
 };
